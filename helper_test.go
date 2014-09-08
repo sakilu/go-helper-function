373d6c611732eb2904de1a2a060e0c5d3c9ffc9e
sakilu <sakilu@gmail.com>
2014-09-09 00:25:00 +0800

    單元測試

--- helper_test.go
+++ helper_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+func TestLog(t *testing.T) {
+	t.Skip()
+}
+
+func BenchmarkLog(b *testing.B) {
+	b.Skip()
+}
+
 func TestUniqid(t *testing.T) {
 
 }
@@ -14,22 +22,38 @@ func BenchmarkUniqid(b *testing.B) {
 	}
 }
 
-func TestLog(t *testing.T) {
-
-}
-
-func BenchmarkLog(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Log(i)
-	}
-}
-
 func TestParseSearchString(t *testing.T) {
 
 }
 
 func TestJoinInt(t *testing.T) {
+	ints := []int{1, 2, 3, 4}
 
+	glue := ","
+	if JoinInt(ints, glue) != "1,2,3,4" {
+		t.Error(JoinInt(ints, glue), " != ", "1,2,3,4")
+	}
+	glue = " ,"
+	if JoinInt(ints, glue) != "1 ,2 ,3 ,4" {
+		t.Error(JoinInt(ints, glue), " != ", "1 ,2 ,3 ,4")
+	}
+	glue = ""
+	if JoinInt(ints, glue) != "1234" {
+		t.Error(JoinInt(ints, glue), " != ", "1234")
+	}
+	glue = "/"
+	if JoinInt(ints, glue) != "1/2/3/4" {
+		t.Error(JoinInt(ints, glue), " != ", "1/2/3/4")
+	}
+	glue = ","
+	ints = []int{}
+	if JoinInt(ints, glue) != "" {
+		t.Error()
+	}
+	ints = []int{1}
+	if JoinInt(ints, glue) != "1" {
+		t.Error(JoinInt(ints, glue), " != ", 1)
+	}
 }
 
 func TestBase64Encode(t *testing.T) {
